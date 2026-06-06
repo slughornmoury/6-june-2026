d3ebea91859bc9681f5e7693341b9f7bf72385cf
Afia Mourin <slughornmoury@gmail.com>
2026-06-06 18:07:31 +0600

    Add files via upload

---
+++ 6.6.26/main.js
@@ -0,0 +1,136 @@
+// for loop syntax
+// for(expr-1; expe-2; expe-3){
+//     // code block here
+// }
+
+// for(let i=0;i<=50;i++){
+//     console.log(`$(i).Optimus Prime`);
+// }
+
+
+// for(let i=0; i<=100;i++){
+//    if (i%2===0)
+//     console.log(i, "the number is even");
+//    else 
+//     console.log(i, "The number is odd")
+// }
+// let sum = 0;
+// for (let i = 0; i <= 10; i++) {
+//     sum += i;
+//     console.log(sum, "the summation")
+// }
+
+// let sum1= 0;
+// for(let i=0;i<=5;i++){
+//     sum+=i;
+//  console.log(sum1, "sum")
+// }
+
+// while(condition){
+//     // code block
+// }
+// let i = 0;
+// while (i <= 5) {
+//     console.log(i);
+//     i++;
+// }
+// let i = 0;
+// do {
+//     console.log(i);
+//     i++;
+// }
+// while (i <= 10)
+
+
+// // loop break & continue
+// for(let i=1;i<=10;i++){
+//     if(i===3){
+//         break;
+
+//         // after 3 stop printing
+
+
+//     }
+//      console.log(i);
+// }
+
+// for(let i=1;i<=10;i++){
+//     if(i===6 ||i===3||i===7){
+//         continue;
+
+//         // skip 3,6,7 and print rest
+
+
+//     }
+//      console.log(i);
+// }
+//empty array
+// const fruits=[]
+
+// const fruits1=["apple","mango","lichi","banana"]
+
+// for(const fruit of fruits1){
+//     console.log(fruit)
+// }
+
+// const user={
+//     name: "Optimus Prime",
+//     age:1000,
+//     occupation: "Leader"
+
+// }
+// // access only keys
+// for (let key in user){
+// console.log(key);
+// }
+// // access only key values
+// for (let key in user){
+// console.log(user[key]);
+// }
+
+
+// // forEach loop
+// const numbers=[12,30,40,44,60]
+// // function=callback
+// numbers.forEach(function(num){
+// console.log(num);
+// })
+
+// for (i = 0; i <= 5; i++) {
+//     /* this loop will work form 0-5
+//     i=0:initialization,
+//     i<=5:condition, 
+//     i++:increment,
+//     i--:decrement */
+
+//     console.log(i, "meawoooooo!")
+// }
+
+
+// for (i=0;i<=1000;i++){
+//     console.log(i,"hiiii!")
+// }
+
+// for (i=1000;i>=0;i--){
+//     console.log(i,"v!")
+// }
+// for (i=100;i>=0;i--){
+//     console.log(i,"volt!")
+// }
+// let i = 0;
+// while (i <= 5) {
+//     console.log(i, "meawoooooo!")
+//     i++;
+// }
+//  let i = 0;
+//  while(i<=1000){
+//     console.log(i,"Vhoot!👻")
+//     i++;
+//  }
+
+
+leti=10;
+while(i>=1){
+    console.log(i,"petni🤖");
+    i--;
+}
